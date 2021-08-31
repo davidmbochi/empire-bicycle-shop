d7a9ef786522d12775f9364979bc8e1eb0c81b03
davidmbochi <davidmbochi97@gmail.com>
2021-08-31 14:42:12 +0300

    modified controller

--- src/main/java/com/example/backend/controller/HomePageController.java
+++ src/main/java/com/example/backend/controller/HomePageController.java
@@ -2,11 +2,12 @@ package com.example.backend.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomePageController {
 
-    @GetMapping("/")
+    @RequestMapping("/")
     public String homePage(){
         return "homepage/home";
     }
